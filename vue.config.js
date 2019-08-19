74340b86a800451f7ff5957376a1f6d4c3e4b997
qzssl <2597253199@qq.com>
2019-08-19 17:20:56 +0800

    修改vue.config.js文件

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
-import path from 'path'
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js'
-  }
+  publicPath: '/',
+  outputDir: './dist'
 }
