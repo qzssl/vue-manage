824528da54435918a8927ab9a88a11a8efc42398
qzssl <2597253199@qq.com>
2019-08-27 14:57:11 +0800

    文本编辑

--- src/components/textedit/TextEdit.vue
+++ src/components/textedit/TextEdit.vue
@@ -1,11 +1,58 @@
 <template>
-  <div>textedit</div>
+  <div class="text-editor">
+    <div class="edit_container">
+      <quill-editor 
+        v-model="content" 
+        ref="myQuillEditor"
+        class="editer"
+        :options="editorOption">
+      </quill-editor>
+    </div>
+    <div class="submit_btn">
+      <el-button type="primary" @click="submit">提交</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { quillEditor } from "vue-quill-editor"; //调用编辑器
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
 export default {
-  components: {}
+    components: {
+        quillEditor
+    },
+    data() {
+        return {
+            content: `<h3>文本编辑</h3>`,
+            editorOption: {}
+        }
+    },
+    methods: {
+        submit(){
+          this.$message.success('提交成功！');
+        }
+    },
+    computed: {
+        editor() {
+            return this.$refs.myQuillEditor.quill;
+        },
+    }
 }
+
 </script>
 <style scoped>
+.submit_btn{
+  text-align: center;
+}
+</style>
+<style>
+.text-editor .edit_container{
+  padding: 40px;
+  margin-bottom: 40px;
+}
+.text-editor .editer{
+		height: 350px;
+	}
 </style>
